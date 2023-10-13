9c150b1493a9c481456028be54934dde48fe16ba
Maximilian Huber <max_hub@gmx.net>
2023-10-13 18:29:25 +0200

    added basic analysis

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 	"html/template"
 	"encoding/json"
 	"sort"
+	"fmt"
 )
 
 type TravelItem struct {
@@ -32,7 +33,7 @@ func main() {
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/index.html")
+	tmpl, err := template.ParseFiles("index.html")
 	if err != nil {
 		http.Error(w, "Could not load template", http.StatusInternalServerError)
 		return
@@ -48,6 +49,8 @@ func processHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
+		fmt.Println("Received form data:", r.PostForm)
+
 		names := r.PostForm["Name[]"]
 		amounts := r.PostForm["Amount[]"]
 		weights := r.PostForm["Weight[]"]
@@ -56,21 +59,53 @@ func processHandler(w http.ResponseWriter, r *http.Request) {
 		priorities := r.PostForm["priority[]"]
 		bagTypes := r.PostForm["bagtype[]"]
 
+		fmt.Println("Names length:", len(names))
+		fmt.Println("Amounts length:", len(amounts))
+		fmt.Println("Weights length:", len(weights))
+		fmt.Println("Categories length:", len(categories))
+		fmt.Println("Subcategories length:", len(subcategories))
+		fmt.Println("Priorities length:", len(priorities))
+		fmt.Println("BagTypes length:", len(bagTypes))
+
 		var items []TravelItem
 
-		for i, _ := range names {
-			item := TravelItem{
-				Name:        strings.TrimSpace(names[i]),
-				Amount:      atoi(amounts[i], 0),
-				Weight:      atoi(weights[i], 0),
-				Category:    categories[i],
-				Subcategory: strings.TrimSpace(subcategories[i]),
-				Priority:    atoi(priorities[i], 1),
-				BagType:     atoi(bagTypes[i], 1),
+		i := 1
+		for {
+			nameKey := fmt.Sprintf("Name%d", i)
+			if _, ok := r.PostForm[nameKey]; !ok {
+				break
 			}
+
+			item := TravelItem{
+				Name:        strings.TrimSpace(r.PostFormValue(fmt.Sprintf("Name%d", i))),
+				Amount:      atoi(r.PostFormValue(fmt.Sprintf("Amount%d", i)), 1),
+				Weight:      atoi(r.PostFormValue(fmt.Sprintf("Weight%d", i)), 0),
+				Category:    r.PostFormValue(fmt.Sprintf("Category%d", i)),
+				Subcategory: strings.TrimSpace(r.PostFormValue(fmt.Sprintf("Subcategory%d", i))),
+				Priority:    atoi(r.PostFormValue(fmt.Sprintf("Priority%d", i)), 1),
+				BagType:     atoi(r.PostFormValue(fmt.Sprintf("BagType%d", i)), 1),
+			}
+
 			items = append(items, item)
+			i++
 		}
-		sendJSON(w, items)
+		
+		fmt.Println("Processed items:", items)
+
+		results := dataProcessing(items)
+
+		fmt.Println("Analysis results:", results)
+
+		tmpl, err := template.ParseFiles("analysis.html")
+		if err != nil {
+			http.Error(w, "could not load template", http.StatusInternalServerError)
+			return
+		}
+
+		err = tmpl.Execute(w, results)
+		if err != nil {
+			http.Error(w, "Error executing template"+err.Error(), http.StatusInternalServerError)
+		}
 	}
 }
 
