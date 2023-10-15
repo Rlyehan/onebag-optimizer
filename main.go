99aee5d76efcbf4aca868622f4fc9f84fd2179bf
Maximilian Huber <max_hub@gmx.net>
2023-10-15 09:51:52 +0200

    reworked item parsing to fit html form

--- main.go
+++ main.go
@@ -8,7 +8,12 @@ import (
 	"encoding/json"
 	"sort"
 	"fmt"
+	"github.com/go-echarts/go-echarts/v2/charts"
+	"github.com/go-echarts/go-echarts/v2/opts"
+	"bytes"
 )
+
+var itemsList []TravelItem
 
 type TravelItem struct {
 	Name        string  `json:"name"`
@@ -26,14 +31,21 @@ type AnalysisResult struct {
 	BagWeights           map[int]int
 }
 
+type CategoryWeight struct {
+	Category	string
+	Weight		int
+}
+
 func main() {
 	http.HandleFunc("/", rootHandler)
+	http.HandleFunc("/add", addItemHandler)
 	http.HandleFunc("/process", processHandler)
+	http.HandleFunc("/download", downloadHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func rootHandler(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("index.html")
+	tmpl, err := template.ParseFiles("./static/index.html")
 	if err != nil {
 		http.Error(w, "Could not load template", http.StatusInternalServerError)
 		return
@@ -41,71 +53,39 @@ func rootHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl.Execute(w, nil)
 }
 
+func downloadHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	items, err := parseTravelItems(r)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	sendJSON(w, items)
+}
+
 func processHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method == http.MethodPost {
-		err := r.ParseForm()
-		if err != nil {
-			http.Error(w, "Could not parse form", http.StatusBadRequest)
-			return
-		}
+	if r.Method != http.MethodPost {
+		http.Error(w, "Only POST method is allowed", http.StatusMethodNotAllowed)
+		return
+	}
 
-		fmt.Println("Received form data:", r.PostForm)
+	items := itemsList
+	results := dataProcessing(items)
 
-		names := r.PostForm["Name[]"]
-		amounts := r.PostForm["Amount[]"]
-		weights := r.PostForm["Weight[]"]
-		categories := r.PostForm["Category[]"]
-		subcategories := r.PostForm["subcategory[]"]
-		priorities := r.PostForm["priority[]"]
-		bagTypes := r.PostForm["bagtype[]"]
+	tmpl, err := template.ParseFiles("./templates//analysis.html")
+	if err != nil {
+		http.Error(w, "could not load template", http.StatusInternalServerError)
+		return
+	}
 
-		fmt.Println("Names length:", len(names))
-		fmt.Println("Amounts length:", len(amounts))
-		fmt.Println("Weights length:", len(weights))
-		fmt.Println("Categories length:", len(categories))
-		fmt.Println("Subcategories length:", len(subcategories))
-		fmt.Println("Priorities length:", len(priorities))
-		fmt.Println("BagTypes length:", len(bagTypes))
-
-		var items []TravelItem
-
-		i := 1
-		for {
-			nameKey := fmt.Sprintf("Name%d", i)
-			if _, ok := r.PostForm[nameKey]; !ok {
-				break
-			}
-
-			item := TravelItem{
-				Name:        strings.TrimSpace(r.PostFormValue(fmt.Sprintf("Name%d", i))),
-				Amount:      atoi(r.PostFormValue(fmt.Sprintf("Amount%d", i)), 1),
-				Weight:      atoi(r.PostFormValue(fmt.Sprintf("Weight%d", i)), 0),
-				Category:    r.PostFormValue(fmt.Sprintf("Category%d", i)),
-				Subcategory: strings.TrimSpace(r.PostFormValue(fmt.Sprintf("Subcategory%d", i))),
-				Priority:    atoi(r.PostFormValue(fmt.Sprintf("Priority%d", i)), 1),
-				BagType:     atoi(r.PostFormValue(fmt.Sprintf("BagType%d", i)), 1),
-			}
-
-			items = append(items, item)
-			i++
-		}
-		
-		fmt.Println("Processed items:", items)
-
-		results := dataProcessing(items)
-
-		fmt.Println("Analysis results:", results)
-
-		tmpl, err := template.ParseFiles("analysis.html")
-		if err != nil {
-			http.Error(w, "could not load template", http.StatusInternalServerError)
-			return
-		}
-
-		err = tmpl.Execute(w, results)
-		if err != nil {
-			http.Error(w, "Error executing template"+err.Error(), http.StatusInternalServerError)
-		}
+	err = tmpl.Execute(w, results)
+	if err != nil {
+		http.Error(w, "Error executing template: "+err.Error(), http.StatusInternalServerError)
 	}
 }
 
@@ -115,6 +95,28 @@ func atoi(s string, defaultValue int) int {
 		return defaultValue
 	}
 	return value
+}
+
+func addItemHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "only POST method is allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	item := TravelItem{
+		Name:        r.FormValue("itemName"),
+		Amount:      atoi(r.FormValue("itemAmount"), 1),
+		Weight:      atoi(r.FormValue("itemWeight"), 0),
+		Category:    r.FormValue("itemCategory"),
+		Subcategory: r.FormValue("itemSubcategory"),
+		Priority:    atoi(r.FormValue("itemPriority"), 1),
+		BagType:     atoi(r.FormValue("itemBagType"), 1),
+	}
+
+	itemsList = append(itemsList, item)
+
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Item added")
 }
 
 func generateJSON(items []TravelItem) ([]byte, error) {
@@ -159,4 +161,77 @@ func dataProcessing(items []TravelItem) AnalysisResult {
 		TopHeaviestItems:     topHeaviestItems,
 		BagWeights:           bagWeights,
 	}
+}
+
+func parseTravelItems(r *http.Request) ([]TravelItem, error) {
+	err := r.ParseForm()
+	if err != nil {
+		return nil, fmt.Errorf("error parsing data: %v", err)
+	}
+
+	var items []TravelItem
+	i := 1
+	for {
+		nameKey := fmt.Sprintf("Name%d", i)
+		if _, ok := r.PostForm[nameKey]; !ok {
+			break
+		}
+
+		item := TravelItem{
+			Name:        strings.TrimSpace(r.PostFormValue(nameKey)),
+			Amount:      atoi(r.PostFormValue(fmt.Sprintf("Amount%d", i)), 1),
+			Weight:      atoi(r.PostFormValue(fmt.Sprintf("Weight%d", i)), 0),
+			Category:    r.PostFormValue(fmt.Sprintf("Category%d", i)),
+			Subcategory: strings.TrimSpace(r.PostFormValue(fmt.Sprintf("Subcategory%d", i))),
+			Priority:    atoi(r.PostFormValue(fmt.Sprintf("Priority%d", i)), 1),
+			BagType:     atoi(r.PostFormValue(fmt.Sprintf("BagType%d", i)), 1),
+		}
+
+		items = append(items, item)
+		i++
+	}
+
+	return items, nil
+}
+
+func generatePieItems(data []CategoryWeight) []opts.PieData {
+	var items []opts.PieData
+	for _, item := range data {
+		items = append(items, opts.PieData{Name: item.Category, Value: item.Weight})
+	}
+	return items
+}
+
+func categoryWeightPieChart(data []CategoryWeight) string {
+	pie := charts.NewPie()
+	pie.SetGlobalOptions(
+		charts.WithTitleOpts(
+			opts.Title{
+				Title:    "Weight distribution per Category",
+				Subtitle: "Analyzed from travel items",
+			},
+		),
+	)
+	pie.SetSeriesOptions()
+	pie.AddSeries("Category Weights",
+		generatePieItems(data)).
+		SetSeriesOptions(
+			charts.WithPieChartOpts(
+				opts.PieChart{
+					Radius: 200,
+				},
+			),
+			charts.WithLabelOpts(
+				opts.Label{
+					Show:      true,
+					Formatter: "{b}: {c}",
+				},
+			),
+		)
+		buffer := new(bytes.Buffer)
+		err := pie.Render(buffer)
+		if err != nil {
+			return "Could not generate the Pie Chart."
+		}
+		return buffer.String()
 }
--- main_test.go
+++ main_test.go
@@ -3,6 +3,10 @@ package main
 import (
 	"reflect"
 	"testing"
+	"os"
+	"io/ioutil"
+	"strings"
+	"net/http/httptest"
 )
 
 func TestDataProcessing(t *testing.T) {
@@ -34,5 +38,65 @@ func TestDataProcessing(t *testing.T) {
 
 	if !reflect.DeepEqual(result.BagWeights, expectedBagWeights) {
 		t.Errorf("Expected bag weights to be %+v but got %+v", expectedBagWeights, result.BagWeights)
+	}
+}
+
+func TestCategoryWeightPieChart(t *testing.T) {
+	data := []CategoryWeight{
+		{Category: "Clothing", Weight: 2000},
+		{Category: "Electronics", Weight: 1500},
+		{Category: "Grooming", Weight: 1200},
+	}
+
+	htmlContent := categoryWeightPieChart(data)
+
+	err := ioutil.WriteFile("pie.html", []byte(htmlContent), 0644)
+	if err != nil {
+		t.Fatalf("Error writing to pie.html: %v", err)
+	}
+
+	if _, err := os.Stat("pie.html"); os.IsNotExist(err) {
+		t.Fatalf("Expected pie.html to be generated, but it wasn't.")
+	}
+
+	content, err := ioutil.ReadFile("pie.html")
+	if err != nil {
+		t.Fatalf("Could not read pie.html: %v", err)
+	}
+
+	if !strings.Contains(string(content), "Weight distribution per Category") {
+		t.Fatalf("Expected title not found in pie.html")
+	}
+}
+
+func TestAddItemHandler(t *testing.T) {
+	itemsList = []TravelItem{}
+
+	r1 := httptest.NewRequest("POST", "/add", strings.NewReader("itemName=TestItem1&itemAmount=1&itemWeight=100&itemCategory=Bags&itemSubcategory=Backpack&itemPriority=1&itemBagType=1"))
+	r1.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	w1 := httptest.NewRecorder()
+
+	addItemHandler(w1, r1)
+
+	r2 := httptest.NewRequest("POST", "/add", strings.NewReader("itemName=TestItem2&itemAmount=2&itemWeight=200&itemCategory=Electronics&itemSubcategory=Camera&itemPriority=2&itemBagType=2"))
+	r2.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	w2 := httptest.NewRecorder()
+
+	addItemHandler(w2, r2)
+
+	if len(itemsList) != 2 {
+		t.Fatalf("Expected 2 items in itemsList, but got %d", len(itemsList))
+	}
+
+	item1 := itemsList[0]
+	if item1.Name != "TestItem1" {
+		t.Errorf("Expected first item name to be TestItem1, but got %s", item1.Name)
+	}
+
+	item2 := itemsList[1]
+	if item2.Name != "TestItem2" {
+		t.Errorf("Expected second item name to be TestItem2, but got %s", item2.Name)
 	}
 }
