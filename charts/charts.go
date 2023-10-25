7f24279ebf59590b480e8a7b40aea05c0e333974
Maximilian Huber <max_hub@gmx.net>
2023-10-25 09:27:48 +0200

    Update chart creation to chartsjs

--- charts/charts.go
+++ charts/charts.go
@@ -1,99 +1,120 @@
 package charts
 
 import (
-	"github.com/go-echarts/go-echarts/v2/charts"
-	"github.com/go-echarts/go-echarts/v2/opts"
-	"github.com/PuerkitoBio/goquery"
-	chartrender "github.com/go-echarts/go-echarts/v2/render"
-	"github.com/Rlyehan/onebag-optimizer/models"
-	"html/template"
 	"bytes"
-	"log"
-	"strings"
-	"html"
-	"regexp"
-	"fmt"
+	"html/template"
+
+	"github.com/Rlyehan/onebag-optimizer/models"
 )
 
-
-func generatePieItems(data []models.CategoryWeight) []opts.PieData {
-	var items []opts.PieData
-	for _, item := range data {
-		items = append(items, opts.PieData{Name: item.Category, Value: item.Weight})
-	}
-	return items
+type DataPoint struct {
+	Label string  `json:"label"`
+	Value float64 `json:"value"`
 }
 
-func decodeHtmlEntities(input string) string {
-	return html.UnescapeString(input)
+type StackedBarDataPoint struct {
+	Label string    `json:"label"`
+	Data  []float64 `json:"data"`
 }
 
-func renderToHtml(c interface{}) template.HTML {
-    var buf bytes.Buffer
-    r := c.(chartrender.Renderer)
-    err := r.Render(&buf)
-    if err != nil {
-        log.Printf("Failed to render chart: %s", err)
-        return ""
-    }
-
-    htmlString := buf.String()
-    doc, err := goquery.NewDocumentFromReader(strings.NewReader(htmlString))
-    if err != nil {
-        log.Printf("Failed to parse HTML: %s", err)
-        return ""
-    }
-
-    // Extract the chart ID from the rendered script
-    chartId := doc.Find("script").Text()
-    re := regexp.MustCompile(`document.getElementById\('(.+?)'\)`)
-    matches := re.FindStringSubmatch(chartId)
-    if len(matches) < 2 {
-        log.Printf("Failed to extract chart ID")
-        return ""
-    }
-    extractedChartId := matches[1]
-
-    script, _ := doc.Find("body script").Html()
-	script = decodeHtmlEntities(script)
-
-    cleanedHtml := fmt.Sprintf(`<div class="item" id="%s" style="width:900px;height:500px;"></div>
-<script type="text/javascript">%s</script>`, extractedChartId, script)
-
-    return template.HTML(cleanedHtml)
+type ChartData struct {
+	ID     string    `json:"id"`
+	Labels []string  `json:"labels"`
+	Label  string    `json:"label"`
+	Data   []float64 `json:"data"`
 }
 
+type StackedBarData struct {
+	ID       string                `json:"id"`
+	Labels   []string              `json:"labels"`
+	Datasets []StackedBarDataPoint `json:"datasets"`
+}
 
-func CategoryWeightPieChart(data []models.CategoryWeight) (template.HTML, error) {
-	pie := charts.NewPie()
-	pie.SetGlobalOptions(
-		charts.WithTitleOpts(
-			opts.Title{
-				Title:    "Weight distribution per Category",
-				Subtitle: "Analyzed from travel items",
-			},
-		),
-	)
-	pie.SetSeriesOptions()
-	pie.AddSeries("Category Weights",
-		generatePieItems(data)).
-		SetSeriesOptions(
-			charts.WithPieChartOpts(
-				opts.PieChart{
-					Radius: 200,
-				},
-			),
-			charts.WithLabelOpts(
-				opts.Label{
-					Show:      true,
-					Formatter: "{b}: {c}",
-				},
-			),
-		)
-	buffer := new(bytes.Buffer)
-	err := pie.Render(buffer)
+func CreatePieChart(id string, dataPoints []DataPoint) (template.HTML, error) {
+	tmpl, err := template.ParseFiles("templates/pieChart.html")
 	if err != nil {
 		return "", err
 	}
-	return renderToHtml(pie), nil
+
+	labels := make([]string, len(dataPoints))
+	data := make([]float64, len(dataPoints))
+	for i, dp := range dataPoints {
+		labels[i] = dp.Label
+		data[i] = dp.Value
+	}
+
+	chartData := ChartData{
+		ID:     id,
+		Labels: labels,
+		Label:  "Category Weights",
+		Data:   data,
+	}
+
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, chartData)
+	if err != nil {
+		return "", err
+	}
+
+	return template.HTML(buf.String()), nil
+}
+
+func CreateStackedBarChart(id string, labels []string, datasets []StackedBarDataPoint) (template.HTML, error) {
+	tmpl, err := template.ParseFiles("templates/stackedBarChart.html")
+	if err != nil {
+		return "", err
+	}
+
+	stackedBarData := StackedBarData{
+		ID:       id,
+		Labels:   labels,
+		Datasets: datasets,
+	}
+
+	var buf bytes.Buffer
+	err = tmpl.Execute(&buf, stackedBarData)
+	if err != nil {
+		return "", err
+	}
+
+	return template.HTML(buf.String()), nil
+}
+
+func GenerateStackedBarData(id string, items []models.TravelItem) StackedBarData {
+	bagTypeCategoryMap := make(map[string]map[string]float64)
+	var bagTypesMap = make(map[string]bool)
+	var categoriesMap = make(map[string]bool)
+
+	for _, item := range items {
+		if _, exists := bagTypeCategoryMap[item.BagType]; !exists {
+			bagTypeCategoryMap[item.BagType] = make(map[string]float64)
+		}
+		bagTypeCategoryMap[item.BagType][item.Category] += float64(item.Weight)
+
+		bagTypesMap[item.BagType] = true
+		categoriesMap[item.Category] = true
+	}
+
+	bagTypes := make([]string, 0, len(bagTypesMap))
+	for bagType := range bagTypesMap {
+		bagTypes = append(bagTypes, bagType)
+	}
+
+	var datasets []StackedBarDataPoint
+	for category := range categoriesMap {
+		data := make([]float64, len(bagTypes))
+		for i, bagType := range bagTypes {
+			data[i] = bagTypeCategoryMap[bagType][category]
+		}
+		datasets = append(datasets, StackedBarDataPoint{
+			Label: category,
+			Data:  data,
+		})
+	}
+
+	return StackedBarData{
+		ID:       id,
+		Labels:   bagTypes,
+		Datasets: datasets,
+	}
 }
--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,98 +1,110 @@
 package handlers
 
 import (
+	"bytes"
 	"encoding/json"
-	"html/template"
-	"net/http"
 	"fmt"
+	"html/template"
+	"io"
 	"log"
+	"net/http"
+
+	"github.com/Rlyehan/onebag-optimizer/charts"
 	"github.com/Rlyehan/onebag-optimizer/models"
 	"github.com/Rlyehan/onebag-optimizer/processor"
-	"github.com/Rlyehan/onebag-optimizer/charts"
 	"github.com/Rlyehan/onebag-optimizer/utils"
-	"io/ioutil"
-    "bytes"
 )
 
 func RootHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl, err := template.ParseFiles("./static/index.html")
 	if err != nil {
-        utils.HandleError(w, http.StatusInternalServerError, err, "Failed to load template")
-        return
+		utils.HandleError(w, http.StatusInternalServerError, err, "Failed to load template")
+		return
 	}
 	if err = tmpl.Execute(w, nil); err != nil {
-        utils.HandleError(w, http.StatusInternalServerError, err, "Error executing template")
+		utils.HandleError(w, http.StatusInternalServerError, err, "Error executing template")
 		return
-    }
+	}
 }
 
 func ProcessHandler(w http.ResponseWriter, r *http.Request) {
-    log.Println("Entered ProcessHandler")
+	log.Println("Entered ProcessHandler")
 
-    // Log the raw payload
-    bodyBytes, _ := ioutil.ReadAll(r.Body)
-    log.Printf("Received payload: %s", bodyBytes)
-    r.Body = ioutil.NopCloser(bytes.NewBuffer(bodyBytes))  // Reset the body so it can be read again
+	bodyBytes, _ := io.ReadAll(r.Body)
+	log.Printf("Received payload: %s", bodyBytes)
+	r.Body = io.NopCloser(bytes.NewBuffer(bodyBytes))
 
-    if r.Method != http.MethodPost {
-        log.Println("Method is not POST")
-        utils.HandleError(w, http.StatusMethodNotAllowed, fmt.Errorf("Method not allowed"), "Only POST method is allowed")
-        return
-    }
+	if r.Method != http.MethodPost {
+		log.Println("Method is not POST")
+		utils.HandleError(w, http.StatusMethodNotAllowed, fmt.Errorf("Method not allowed"), "Only POST method is allowed")
+		return
+	}
 
-    var items []models.TravelItem
-    err := json.NewDecoder(r.Body).Decode(&items)
-    if err != nil {
-        log.Printf("JSON decoding error: %v", err)
-        utils.HandleError(w, http.StatusBadRequest, err, "Error decoding JSON")
-        return
-    }
+	defer r.Body.Close()
 
-    log.Printf("Decoded %d items", len(items))
+	var items []models.TravelItem
+	err := json.NewDecoder(r.Body).Decode(&items)
+	if err != nil {
+		log.Printf("JSON decoding error: %v", err)
+		utils.HandleError(w, http.StatusBadRequest, err, "Error decoding JSON")
+		return
+	}
 
-    for _, item := range items {
-        if !processor.IsValidTravelItem(item) {
-            log.Printf("Invalid item detected: %+v", item)
-            utils.HandleError(w, http.StatusUnprocessableEntity, fmt.Errorf("Invalid travel item data"), "Invalid travel item data")
-            return
-        }
-    }
+	log.Printf("Decoded %d items", len(items))
 
-    results := processor.DataProcessing(items)
+	for _, item := range items {
+		if !processor.IsValidTravelItem(item) {
+			log.Printf("Invalid item detected: %+v", item)
+			utils.HandleError(w, http.StatusUnprocessableEntity, fmt.Errorf("Invalid travel item data"), "Invalid travel item data")
+			return
+		}
+	}
 
-    var categoryWeights []models.CategoryWeight
-    for category, weight := range results.CategoryWeights {
-        categoryWeights = append(categoryWeights, models.CategoryWeight{
-            Category: category,
-            Weight:   weight,
-        })
-    }
+	results := processor.DataProcessing(items)
 
-    chart, err := charts.CategoryWeightPieChart(categoryWeights)
-    if err != nil {
-        log.Printf("Pie chart generation error: %v", err)
-        utils.HandleError(w, http.StatusInternalServerError, err, "Failed to generate the pie chart")
-        return
-    }
+	var categoryWeights []charts.DataPoint
+	for category, weight := range results.CategoryWeights {
+		categoryWeights = append(categoryWeights, charts.DataPoint{
+			Label: category,
+			Value: float64(weight),
+		})
+	}
 
-    data := struct {
-        Analysis models.AnalysisResult
-        Chart    template.HTML
-    }{
-        Analysis: results,
-        Chart:    chart,
-    }
+	pieChart, err := charts.CreatePieChart("CategoryWeightsPieChart", categoryWeights)
+	if err != nil {
+		log.Printf("Pie chart generation error: %v", err)
+		utils.HandleError(w, http.StatusInternalServerError, err, "Failed to generate the pie chart")
+		return
+	}
 
-    tmpl, err := template.ParseFiles("./templates/analysis.html")
-    if err != nil {
-        log.Printf("Template loading error: %v", err)
-        utils.HandleError(w, http.StatusInternalServerError, err, "Could not load template")
-        return
-    }
+	stackedBarData := charts.GenerateStackedBarData("CategoryWeightsBarChart", items)
+	stackedBarChart, err := charts.CreateStackedBarChart(stackedBarData.ID, stackedBarData.Labels, stackedBarData.Datasets)
+	if err != nil {
+		log.Printf("Stacked bar chart generation error: %v", err)
+		utils.HandleError(w, http.StatusInternalServerError, err, "Failed to generate the stacked bar chart")
+		return
+	}
 
-    err = tmpl.Execute(w, data)
-    if err != nil {
-        log.Printf("Template execution error: %v", err)
-        utils.HandleError(w, http.StatusInternalServerError, err, "Error executing template")
-    }
+	data := struct {
+		Analysis        models.AnalysisResult
+		PieChart        template.HTML
+		StackedBarChart template.HTML
+	}{
+		Analysis:        results,
+		PieChart:        pieChart,
+		StackedBarChart: stackedBarChart,
+	}
+
+	tmpl, err := template.ParseFiles("./templates/analysis.html")
+	if err != nil {
+		log.Printf("Template loading error: %v", err)
+		utils.HandleError(w, http.StatusInternalServerError, err, "Could not load template")
+		return
+	}
+
+	err = tmpl.Execute(w, data)
+	if err != nil {
+		log.Printf("Template execution error: %v", err)
+		utils.HandleError(w, http.StatusInternalServerError, err, "Error executing template")
+	}
 }
